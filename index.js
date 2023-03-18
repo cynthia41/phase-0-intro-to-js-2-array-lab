9f400bcc44e665a11234154a77537882e98d20b3
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-03-18 11:06:57 +0300

    Completed Array la

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Write your solution here!
 const cats=["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
 
 function destructivelyAppendCat(name){
          cats.push(name);
